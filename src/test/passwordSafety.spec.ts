b4490f0a449fdb0a8c8aaee2e54a51092a3ce291
Mokha Lerthsuwanroj <70966063+namolert@users.noreply.github.com>
2023-04-16 15:17:40 +0700

    Create test for passwordSafety and wageCalculator

--- src/test/passwordSafety.spec.ts
+++ src/test/passwordSafety.spec.ts
@@ -7,5 +7,34 @@ describe("passwordSafety", () => {
     );
   });
 
-  //...
+  it("Case 2: Password isn't contain any number", () => {
+    expect(passwordSafety("@Bcdefgh")).toStrictEqual(
+      "Password should contain at least 1 number"
+    );
+  });
+
+  it("Case 3: Password isn't contain any uppercase character", () => {
+    expect(passwordSafety("@2cdefgh")).toStrictEqual(
+      "Password should contain at least 1 uppercase character"
+    );
+  });
+
+  it("Case 4: Password isn't contain any lowercase character", () => {
+    expect(passwordSafety("@2CDEFGH")).toStrictEqual(
+      "Password should contain at least 1 lowercase character"
+    );
+  });
+
+  it("Case 5: Password isn't contain any special character", () => {
+    expect(passwordSafety("A2cdefgh")).toStrictEqual(
+      "Password should contain at least 1 special character"
+    );
+  });
+
+  it("Case 6: Password is safe", () => {
+    expect(passwordSafety("@2Cdefgh")).toStrictEqual(
+      "Password is safe (a little bit)"
+    );
+  });
+
 });
--- src/test/wageCalculator.spec.ts
+++ src/test/wageCalculator.spec.ts
@@ -1,9 +1,79 @@
 import { wageCalculator } from "../wageCalculator";
 
 describe("wageCalculator", () => {
-  it("Case 1: Not a leap year, mon, wed, no work level", () => {
+  it("Case 1: Not a leap year, mon, wed", () => {
     expect(wageCalculator(2023, ["mon", "wed"])).toStrictEqual(170);
   });
 
-  //...
+  it("Case 2: Not a leap year, mon, thu", () => {
+    expect(wageCalculator(2023, ["mon", "thu"])).toStrictEqual(170);
+  });
+
+  it("Case 3: Not a leap year, mon, tue, no work level", () => {
+    expect(wageCalculator(2023, ["mon", "tue"])).toStrictEqual(180);
+  });
+
+  it("Case 4: Not a leap year, mon, tue, work level director", () => {
+    expect(wageCalculator(2023, ["mon", "tue"], "director")).toStrictEqual(210);
+  });
+
+  it("Case 5: Not a leap year, mon, tue, work level manager", () => {
+    expect(wageCalculator(2023, ["mon", "tue"], "manager")).toStrictEqual(190);
+  });
+
+  it("Case 6: Not a leap year, mon, fri, no work level", () => {
+    expect(wageCalculator(2023, ["mon", "fri"])).toStrictEqual(310);
+  });
+
+  it("Case 7: Not a leap year, mon, fri,  have work level", () => {
+    expect(wageCalculator(2023, ["mon", "fri"], "director")).toStrictEqual(190);
+  });
+
+  it("Case 8: Not a leap year, mon, sat", () => {
+    expect(wageCalculator(2023, ["mon", "sat"])).toStrictEqual(300);
+  });
+
+  it("Case 9: Not a leap year, mon, sun", () => {
+    expect(wageCalculator(2023, ["mon", "sun"])).toStrictEqual(300);
+  });
+
+  it("Case 10: No work days", () => {
+    expect(wageCalculator(2023, [""])).toStrictEqual(0);
+  });
+
+  it("Case 11: Leap year, mon, wed", () => {
+    expect(wageCalculator(2024, ["mon", "wed"])).toStrictEqual(204);
+  });
+
+  it("Case 12: Leap year, mon, thu", () => {
+    expect(wageCalculator(2024, ["mon", "thu"])).toStrictEqual(204);
+  });
+
+  it("Case 13: Leap year, mon, tue, no work level", () => {
+    expect(wageCalculator(2024, ["mon", "tue"])).toStrictEqual(216);
+  });
+
+  it("Case 14: Leap year, mon, tue, work level director", () => {
+    expect(wageCalculator(2024, ["mon", "tue"], "director")).toStrictEqual(252);
+  });
+
+  it("Case 15: Leap year, mon, tue, work level manager", () => {
+    expect(wageCalculator(2024, ["mon", "tue"], "manager")).toStrictEqual(228);
+  });
+
+  it("Case 16: Leap year, mon, fri, no work level", () => {
+    expect(wageCalculator(2024, ["mon", "fri"])).toStrictEqual(372);
+  });
+
+  it("Case 17: Leap year, mon, fri,  have work level", () => {
+    expect(wageCalculator(2024, ["mon", "fri"], "director")).toStrictEqual(228);
+  });
+
+  it("Case 18: Leap year, mon, sat", () => {
+    expect(wageCalculator(2024, ["mon", "sat"])).toStrictEqual(360);
+  });
+
+  it("Case 19: Leap year, mon, sun", () => {
+    expect(wageCalculator(2024, ["mon", "sun"])).toStrictEqual(360);
+  });
 });
